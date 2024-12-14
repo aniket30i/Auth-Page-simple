ba56f5390d9c4c74c81ca2dbf782a0801a8d1ce9
aniket30i <iamaniket.saha.09@protonmail.com>
2024-12-15 02:06:39 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,7 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+//P9VxZSA8uV1jliVo
+
+//mongodb+srv://aniketsaha1809:P9VxZSA8uV1jliVo@authcluster.ul67c.mongodb.net/?retryWrites=true&w=majority&appName=authCluster
