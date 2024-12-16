b59b8e311cb7a2c996297a4db6133d27504bd5f7
aniket30i <iamaniket.saha.09@protonmail.com>
2024-12-16 13:43:00 +0530

    setting better folder structure

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,14 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("signup route");
-});
-router.get("/login", (req, res) => {
-  res.send("login route");
-});
-router.get("/logout", (req, res) => {
-  res.send("logout route");
-});
+router.get("/signup");
+router.get("/login");
+router.get("/logout");
 
 export default router;
